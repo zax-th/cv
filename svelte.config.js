333608e013757c9c87aabe7ab6c185a90d70ecfb
nakarin weerasak <nakarin.w@odds.team>
2026-02-20 14:34:51 +0700

    feat(): publish

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
       precompress: false,
-      strict: true
-    })
-  }
+      strict: true,
+    }),
+    paths: {
+      base: process.argv.includes("dev") ? "" : "/cv",
+    },
+  },
 };
 
 export default config;
